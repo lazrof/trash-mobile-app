dbc9a99fad781d03c5c05f625a8d145f9a1424d5
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-01-22 23:32:38 -0500

    update models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Trash)
+admin.site.register(TrashState)
+admin.site.register(Schedule)
+admin.site.register(Sector)
+admin.site.register(Feedback)
+
+
--- api/models.py
+++ api/models.py
@@ -2,10 +2,23 @@ from django.db import models
 from django.contrib.auth import get_user_model
 # Create your models here.
 
+class Schedule(models.Model):
+
+	start_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
+	end_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
+	days		= models.CharField(max_length=250, blank=True, null=True)
+
+class Sector(models.Model):
+	#can have many trashes
+	address = models.CharField(blank=True, null=True, max_length=250)
+	lat     = models.CharField(max_length=250)
+	lng     = models.CharField(max_length=250)
+	schedules	= models.ManyToManyField(Schedule, blank=True)
+
 class Trash(models.Model):
 	#can have many states
-	name = models.CharField(max_length=1, blank=True, null=True)
-
+	name = models.CharField(max_length=50, blank=True, null=True)
+	sector   = models.ForeignKey(Sector, on_delete=models.CASCADE)
 
 
 class TrashState(models.Model):
@@ -18,21 +31,6 @@ class TrashState(models.Model):
 	trash       = models.ForeignKey(Trash, on_delete=models.CASCADE)
 	content     = models.CharField(max_length=1, choices=TRASH_CONTENT)
 	timestamp   = models.DateTimeField(auto_now_add=True)
-
-class Schedule(models.Model):
-
-	start_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
-	end_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
-	days		= models.CharField(max_length=250, blank=True, null=True)
-
-
-class Sector(models.Model):
-	#can have many trashes
-	address = models.CharField(blank=True, null=True, max_length=250)
-	lat     = models.CharField(max_length=250)
-	lng     = models.CharField(max_length=250)
-	trash   = models.ForeignKey(Trash, on_delete=models.CASCADE)
-	schedules	= models.ManyToManyField(Schedule, blank=True)
 
 
 class Feedback(models.Model):
