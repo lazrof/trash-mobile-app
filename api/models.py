e67c182e724b69a3d78ccec8c4b2cad2a9a26174
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-01-22 23:21:06 -0500

    finish models

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Trash(models.Model):
@@ -19,9 +19,26 @@ class TrashState(models.Model):
 	content     = models.CharField(max_length=1, choices=TRASH_CONTENT)
 	timestamp   = models.DateTimeField(auto_now_add=True)
 
+class Schedule(models.Model):
+
+	start_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
+	end_time	= models.DateTimeField(auto_now_add=False, auto_now=False)
+	days		= models.CharField(max_length=250, blank=True, null=True)
+
+
 class Sector(models.Model):
 	#can have many trashes
 	address = models.CharField(blank=True, null=True, max_length=250)
 	lat     = models.CharField(max_length=250)
 	lng     = models.CharField(max_length=250)
 	trash   = models.ForeignKey(Trash, on_delete=models.CASCADE)
+	schedules	= models.ManyToManyField(Schedule, blank=True)
+
+
+class Feedback(models.Model):
+
+	rating	= models.IntegerField()
+	review	= models.CharField(max_length=300, blank=True, null=True)
+	timestamp   = models.DateTimeField(auto_now_add=True)
+	trash		= models.ForeignKey(Trash, on_delete=models.CASCADE)
+	user		= models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
